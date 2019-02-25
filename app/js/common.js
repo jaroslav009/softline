d60be3900cf4ab5b28ae7d46b2c1d60f95c4ec0e
jaroslav009 <jaroslav.iliuk@gmail.com>
2019-02-25 22:21:50 +0300

    work

---
+++ app/js/common.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('.single-item').slick({
+		dots: true,
+	  	focusOnSelect: true,
+	  	nextArrow: '<div class="arrow-right"><i class="fas fa-angle-right"></i></div>',
+ 		prevArrow: '<div class="arrow-left"><i class="fas fa-angle-left"></i></div>',
+	});
+
+});
