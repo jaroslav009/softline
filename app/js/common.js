ad9e1537a4d81cbfcfd78547c895e44275215955
jaroslav009 <jaroslav.iliuk@gmail.com>
2019-03-02 13:12:44 +0300

    work

--- app/js/common.js
+++ app/js/common.js
@@ -17,15 +17,19 @@ $(function() {
 	var countHamb = 1;
 	$('#hamburger-menu').click(function() {
 		if(countHamb == 1) {
+			countHamb = 0;
 			$('.hamburger').addClass('is-active');
 			$('.head-menu').animate({width:'toggle'},350);
 			$("body").css("overflow","hidden"); 
-			countHamb = 0;
-		} else {
+			
+			console.log('2')
+
+		} else if (countHamb == 0){
 			countHamb = 1;
 			$('.hamburger').removeClass('is-active');
 			$('.head-menu').animate({width:'toggle'},350);
 			$("body").css("overflow","auto");
+			console.log('1')
 		}
 		
 	})
